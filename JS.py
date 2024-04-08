cad7e07f73f6841b4783b0a36f902b4210c8e2b6
AssemblyProgrammer101 <124309800+PyDOS8@users.noreply.github.com>
2024-04-08 13:20:49 -0400

    Update JS.py

--- JS.py
+++ JS.py
@@ -8,8 +8,28 @@ while True:
             print(i)
     elif inp.startswith("exit"):
         exit()
+    elif inp.startswith("repeat"):
+        iterator = input("INC or DEC? ")
+        if iterator == "INC":
+            iteratorName = input("Enter iterator name > ")
+            instruction = "for(var " + iteratorName + " = 0; " + iteratorName + " < " + input("Enter the amount of times you want to increment > ") + "; i++){" + input("Action > ") + "}"
+            instructions.append(instruction)
+        else:
+            iteratorName = input("Enter iterator name > ")
+            instruction = "for(var " + iteratorName + " = 0; " + iteratorName + " < " + input("Enter the amount of times you want to decrement > ") + "; i--){" + input("ACtion > ") + "}"
+            instructions.append(instruction)
+    elif inp.startswith("save"):
+        fileName = inp[5::]
+        if not os.path.exists(fileName):
+            with open(fileName, "w") as wf:
+                for instruction in instructions:
+                    wf.write(instruction)
+                wf.close()
+    elif inp.startswith("infinite"):
+        instruction = "while(true){" + input("Enter what you want to repeat an infinite amount of time > ") + "}"
+        instructions.append(instruction)
     elif inp.startswith("variable"):
-        variableName = inp[8::]
+        variableName = inp[9::]
         variableValue = input("Enter the variable's value > ")
         if variableValue != "":
             instructions.append("var " + variableName + "=" + variableValue)
