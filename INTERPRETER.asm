90c1ed14bb85739b0da26902a59095bae5979341
AssemblyProgrammer101 <124309800+PyDOS8@users.noreply.github.com>
2024-04-15 11:20:33 -0400

    Update INTERPRETER.asm

--- INTERPRETER.asm
+++ INTERPRETER.asm
@@ -1,218 +1,44 @@
-section .text  
+section .text
     global _start
     global _exit
     global _SyntaxError
-    global _Execute
-    global _Add
-    global _Sub
-    global _Mul
+    global _Read
 
 section .data
-    userInp: db dup (0)
-    instructions: db "PRINT", db "EXIT", "ADD", "SUB", "MUL"
+    IAmAnIdiot:  db dup(0)    ; User input
+    IHateMyself: db "PRINT", "EXIT"
+
 _start:
-    extern _scanf
-
-    ; Output "BASIC > "
-
-    ; Setup the needed registers
-    MOV ESI, "BASIC > "
+        extern _scanf
+        extern _printf
     
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 8
-    INT 0x80
+        ; Output `BASIC > `
 
-    ; Get user input
-    MOV ECX, userInp
-    MOV EDX, 20
-    CALL _scanf
+        ; Setup all of the registers needed
+        MOV ESI, "BASIC > "
 
-    ; Read the user input
-    MOV ESI, [userInp]
+        ; Output
+        MOV ECX, ESI
+        MOV EDX, 8
+        CALL _printf
+        
+        ; Get user input
+        MOV ECX, IAmAnIdiot   ; Where to store the input
+        MOV EDX, 12           ; How many characters are allowed.
+        CALL _scanf
 
-    ; Check if the user input is valid
-    CMP ESI, [instructions]
-    JNE _SyntaxError
+        ; Read the user input
+        MOV ESI, [ECX]    ; ESI = *ECX
 
-    CMP ESI, "PRINT"
-    JE _PRINT
-
-    CMP ESI, "EXIT"
-    JE _exit
-
-    CMP ESI, "ADD"
-    JE _Add
-
-    CMP ESI, "SUB"
-    JE _Sub
-
-    CMP ESI, "MUL"
-    JE _Mul
-
-_PRINT:
-
-    ; Setup the register
-    MOV ESI, "Enter > "
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 8
-    INT 0x80
-
-    ; Get input
-    MOV ECX, userInp
-    MOV EDX, 12
-    CALL _scanf
+        ; Check if the input is valid
+        CMP ESI, IHateMyself
+        JNE _SyntaxError
+        JE _Read
 
 _SyntaxError:
-    
-    ; Setup the register
-    MOV ESI, "Syntax Error!"
+    MOV ESI, "Syntax Error "
+    MOV EDX, 12
+    CALL _printf
 
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 13
-    INT 0x80
-
-_exit:
-    MOV EAX, 1
-    XOR EBX, EBX
-    INT 0x80
-
-_ADD:
-
-    ; Setup the register
-    MOV ESI, "Enter a number > "
-    
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get user input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-    ; Store user input
-    MOV EDI, [ECX]
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get user input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-    ; Store user input
-    MOV ESI, [ECX]
-
-    ; Add the result
-    ADD EDI, ESI
-
-    ; Output the result
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, EDI
-    MOV EDX, 4
-    INT 0x80
-
-_SUB:
-
-    ; Setup the register
-    MOV ESI, "Enter a number > "
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-
-    ; Store input
-    MOV EDI, [ECX]
-
-    ; Reset buffer
-    MOV BYTE [userInp], 0
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-    ; Store input
-    MOV ESI, [ECX]
-
-    ; Subtract the result
-    SUB EDI, ESI
-
-    ; Output the result
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, EDI
-    MOV EDX, 4
-    INT 0x80
-
-_Mul:
-    ; Setup the register
-    MOV ESI, "Enter a number > "
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-    ; Store input
-    MOV EDI, [ECX]
-
-    ; Output
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, ESI
-    MOV EDX, 15
-    INT 0x80
-
-    ; Get input
-    MOV ECX, userInp
-    MOV EDX, 4
-    CALL _scanf
-
-    ; Store input
-    MOV ESI, [ECX]
-
-    ; Multiply the result
-    MUL ESI, EDI
+_Read:
+   ; The rest of my crappy code here.
